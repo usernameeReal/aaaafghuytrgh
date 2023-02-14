a34368bf132e5ba7fa573f9b0f1d796cc7250c3e
usewnamwee <86745494+usernameeReal@users.noreply.github.com>
2023-02-14 19:41:37 +0100

    Create test.chip.c

---
+++ src/test.chip.c
@@ -0,0 +1,16 @@
+
+#include "wokwi-api.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct {
+  int x;
+} chip_state_t;
+
+
+void chip_init() {
+  chip_state_t *chip = malloc(sizeof(chip_state_t));
+
+
+}
